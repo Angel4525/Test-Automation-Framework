50b15f16b6dfe92f1dea91a025a71f8d1af9cc3d
joseangelaguirre <ang_elfg@hotmail.com>
2026-02-02 17:57:49 -0700

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
+/*
+Components for Automation framework
+1. Reporting- Will be using Allure and Exlent
+-will be using a configuration which will report which will be reported
+2. Logs- will be using log4J for logging
+3. UI Automation- Selenium will be used.
+4. Assertion/Test- TestNG will be used.
+-executes tests from terminal or  with testNG.xml
+-has in-built reporting capabilities
+-re-try logic(if test fails, reruns failed test)
+5.Data driven testing-JSON, CSV, Excel, Faker
+5. Code quality- Sonar check(checks code duplication, java standards, code smelts)
+
+
+all development code (any java related file) will be part or src/main/java
+any non java file(json,jpeg, png, css) will be kept in resources folder
+any testing code will be under src/test/java
+can create resources file under test to save non java files related to test.
+
+ */
